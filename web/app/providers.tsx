11fe91b8962f4d82ef09417964ec3e47c81bd1da
aahnik <daw@aahnik.dev>
2024-11-14 00:20:12 +0530

    Move providers.tsx and remove config.ts

--- web/app/providers.tsx
+++ web/app/providers.tsx
@@ -1,18 +1,17 @@
 "use client";
 
 import { PrivyProvider } from "@privy-io/react-auth";
-import { PRIVY_APP_ID } from "@/config";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <PrivyProvider
-      appId={PRIVY_APP_ID}
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID}
       config={{
         // Customize Privy's appearance in your app
         appearance: {
           theme: "light",
           accentColor: "#676FFF",
-          logo: "https://your-logo-url",
+          logo: "/eth.png",
         },
         // Create embedded wallets for users who don't have a wallet
         embeddedWallets: {
--- web/config.ts
+++
@@ -1,9 +0,0 @@
-function getEnv(name: string): string {
-  const value = process.env[name];
-  if (!value) {
-    throw new Error(`Missing environment variable: ${name}`);
-  }
-  return value;
-}
-
-export const PRIVY_APP_ID = getEnv("PRIVY_APP_ID");
